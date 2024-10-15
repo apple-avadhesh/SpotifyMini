29bb4168cfe128a08ec3e4e24b28864f4b0e5d2e
Avadhesh Sharma <apple.avadhesh@gmail.com>
2024-10-15 11:11:13 +0530

    Initial Commit

---
+++ SpotifyMini/SpotifyMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyMiniApp.swift
+//  SpotifyMini
+//
+//  Created by Avadhesh Sharma on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
